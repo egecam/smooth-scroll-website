7bb5e4bc8cc9ea650350463a4570a32dd234a8ed
Ege <egeege95@gmail.com>
2021-09-06 18:49:40 +0300

    added components from InfoElements

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,21 +1,68 @@
 import React from "react";
+import { Button } from "../ButtonElements";
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  ColumnOne,
+  ColumnTwo,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  ImgWrap,
+  Img,
+} from "./InfoElements";
 
-const InfoSection = () => {
+const InfoSection = ({
+  lightBg,
+  id,
+  imgStart,
+  topLine,
+  lightText,
+  headline,
+  darkText,
+  description,
+  buttonLabel,
+  img,
+  alt,
+  primary,
+  dark,
+  dark2,
+}) => {
   return (
     <>
-      <InfoContainer id={id}>
+      <InfoContainer lightBg={lightBg} id={id}>
         <InfoWrapper>
-          <InfoRow>
+          <InfoRow imgStart={imgStart}>
             <ColumnOne>
               <TextWrapper>
-                <TopLine>Topline</TopLine>
-                <Heading>Heading</Heading>
-                <Subtitle>Subtitle</Subtitle>
+                <TopLine>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headline}</Heading>
+                <Subtitle darkText={darkText}>{description}</Subtitle>
                 <BtnWrap>
-                  <Button to="home">Button</Button>
+                  <Button
+                    to="home"
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact="true"
+                    offset={-80}
+                    primary={primary ? 1 : 0}
+                    dark={dark ? 1 : 0}
+                    dark2={dark2 ? 1 : 0}
+                  >
+                    {buttonLabel}
+                  </Button>
                 </BtnWrap>
               </TextWrapper>
             </ColumnOne>
+            <ColumnTwo>
+              <ImgWrap>
+                <Img src={img} alt={alt} />
+              </ImgWrap>
+            </ColumnTwo>
           </InfoRow>
         </InfoWrapper>
       </InfoContainer>
