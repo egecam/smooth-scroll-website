2c305172c4f27fab500268847bb49693219ece77
Ege <egeege95@gmail.com>
2021-09-08 18:54:35 +0300

    some bugfix

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -25,11 +25,11 @@ export const ServicesWrapper = styled.div`
   grid-gap: 16px;
   padding: 0 50px;
 
-  @media screen and(max-width: 1000px) {
+  @media screen and (max-width: 1000px) {
     grid-template-columns: 1fr 1fr;
   }
 
-  @media screen and(max-width: 768px) {
+  @media screen and (max-width: 768px) {
     grid-template-columns: 1fr;
     padding: 0 20px;
   }
